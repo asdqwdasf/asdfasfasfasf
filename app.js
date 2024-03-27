2fde7f139029f0eed843eae2f4858bdd1c841aed
FastikProg <126092637+fastikprog@users.noreply.github.com>
2024-03-27 23:32:33 +0400

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ tg.MainButton.color = "#FF00FF";
 
 const btn = document.getElementById("btn");
 const messageBox = document.getElementById("message");
+const photoInput = document.getElementById("photo"); // Get the input field for photo
+
 
 btn.addEventListener("click", function() {
   const message = messageBox.value;
+  const photoFile = photoInput.files[0]; // Get the selected photo file
+
+  console.log(photoFile)
+  
   tg.MainButton.setText("Сообщение отправлено!");
   tg.MainButton.show();
   tg.sendData(message);
