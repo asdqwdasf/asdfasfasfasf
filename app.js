393e8e0bab3862db9d44d64162f411b0a3035351
FastikProg <126092637+fastikprog@users.noreply.github.com>
2024-03-27 23:44:10 +0400

    Update app.js

--- app.js
+++ app.js
@@ -7,16 +7,27 @@ tg.MainButton.color = "#FF00FF";
 
 const btn = document.getElementById("btn");
 const messageBox = document.getElementById("message");
-const photoInput = document.getElementById("photo"); // Get the input field for photo
-
+const photoInput = document.getElementById("photo");
 
 btn.addEventListener("click", function() {
   const message = messageBox.value;
-  const photoFile = photoInput.files[0]; // Get the selected photo file
+  const photoFile = photoInput.files[0];
 
-  console.log(photoFile)
-  
+  console.log(photoFile);
+
   tg.MainButton.setText("Сообщение отправлено!");
   tg.MainButton.show();
-  tg.sendData(message);
+  
+  if (photoFile) {
+    // If photo is selected, convert it to Base64 and send along with the message
+    const reader = new FileReader();
+    reader.onload = function(event) {
+      const photoBase64 = event.target.result;
+      tg.sendData(message, photoBase64);
+    };
+    reader.readAsDataURL(photoFile);
+  } else {
+    // If no photo selected, send only the message
+    tg.sendData(message);
+  }
 });
